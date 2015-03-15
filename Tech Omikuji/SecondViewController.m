618b7fb8aef3884204eec5716f5e5c66148c2a98
Rey <quit9909@icloud.com>
2015-03-16 00:29:06 +0900

    あれれなぜか関連ずけのとこにbackボタンとかが出てこない！

--- Tech Omikuji/SecondViewController.m
+++ Tech Omikuji/SecondViewController.m
@@ -18,11 +18,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    //numberの値によって、TextViewの色を変える
+    if (number > 7) {
+        textView.text = @"大吉！\nおめでとうございます！！\n 恋愛運 ★★★★★\n 勉強運 ★★★★☆\n 健康運 ★★★★★";
+    }else if (number <= 7 && number > 2){
+        textView.text = @"吉！\nいつもどおりの年です。\n 恋愛運 ★★☆☆☆\n 勉強運 ★★★☆☆\n 健康運 ★★★★☆";
+    }else{
+        textView.text = @"凶…\n残念です…\n 恋愛運 ★☆☆☆☆\n 勉強運 ★★★☆☆\n 健康運 ★★☆☆☆";
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)back{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 
